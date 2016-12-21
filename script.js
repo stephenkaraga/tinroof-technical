c8d6bfd445fa70ad98e78db62da0f1e22449799f
stephenkaraga <stephenkaraga@gmail.com>
2016-12-21 02:33:57 -0500

    Finish

---
+++ script.js
@@ -0,0 +1,54 @@
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('mainController', ['$scope' '$http,' function($scope, $http) {
+
+	$http.get("todos").then(function(response) {
+        $scope.toDos = response.data.todos;
+    }, function(err) {
+    	console.log(err);
+    });
+
+	$scope.newTodo = '';
+
+    $scope.add = function(newToDo) {
+    	console.log($scope.newTodo);
+    	if (newToDo) {
+    		$scope.todo = {};
+	    	$scope.todo['name'] = newTodo;
+	    	$scope.toDos = $scope.toDos || [];
+	    	const url = "todo/" + newToDo;
+	    	$http.post(url).then(function(response) {
+	        	$scope.toDos.push($scope.todo);
+	        	$scope.newTodo = '';
+	    	}, function(err) {
+	    		console.log('Error adding ToDo');
+	    		console.log(err);
+	    		$scope.newTodo = '';
+	    	});
+    	}
+    };
+
+    $scope.edit = function(index, name) {
+    	var newObj = {};
+    	newObj[name] = name;
+    	if (name) {
+    		const url = "todo/" + name;
+	    	$http.put(url, newObj).then(function(response) {
+	        	$scope.toDos[index].name = name;
+	    	},
+	    		console.log('Error updating ToDo');
+	    		console.log(err);
+	    	);
+    	};
+    };
+
+    $scope.delete = function(index, name) {
+    	const url = "todo/" + name;
+    	$http.delete(url).then(function(response) {
+        	$scope.toDos.splice(index, 1);
+    	}, function(err) {
+    		console.log('Error deleting Todo');
+    	});
+    };
+
+}]);
---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const monk = require('monk');
+const mongoskin = require('mongoskin');
+const bodyParser = require('body-parser');
+const db = mongoskin.db('mongodb://tinroof:tinroof1@ds141128.mlab.com:41128/tinroof-app', {safe:true});
+db.createCollection('todos');
+
+app.use(function(req, res, next) {
+  req.db = {};
+  req.db.todos = db.collection('todos');
+  next();
+});
+
+app.use(express.static('public'))
+
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'todo' database");
+        db.collection('todos', {strict:true}, function(err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+            	app.listen(3000);
+            }
+        });
+    }
+});
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+app.use(express.static('public'))
+
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+app.get('/todos', getAll);
+app.put('/todos/:name', edit);
+app.post('/todos/:name', add);
+app.delete('/todos/:name', deleteTodo);
+
+function add(req, res) {
+    db.collection('todos').save(req.body, function(err, result) {
+	    if (err) return console.log(err);
+	    console.log('Saved to database');
+  	});
+}
+
+function getAll(req, res) {
+	db.collection('todos').find({}, {}, function(err, docs) {
+		res.json({data:docs});
+	});
+};
+
+function edit(req, res) {
+    db.collection('todos')
+	  .findOneAndUpdate({name: req.params.name}, {
+	    $set: {
+	      name: req.body.newName,
+	    }
+	  }, {
+	    sort: {_id: -1},
+	    upsert: true
+	  }, (err, result) => {
+	    if (err) return res.send(err)
+	    res.send(result)
+	  });
+}
+
+function deleteTodo(req, res) {
+    db.collection('todos').findOneAndDelete({name: req.params.name},
+	  function(err, result) {
+	    if (err) return res.send(500, err)
+	    res.send('Deleted')
+  	});
+}
